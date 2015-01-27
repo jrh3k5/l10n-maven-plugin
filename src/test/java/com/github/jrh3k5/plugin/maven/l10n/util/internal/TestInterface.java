8bd3d9decc5425b7149754cb93c1f1db142f595e
jh016266 <joshua.hyde@cerner.com>
2015-01-26 20:57:49 -0600

    #4: missed some files

---
+++ src/test/java/com/github/jrh3k5/plugin/maven/l10n/util/internal/TestInterface.java
@@ -0,0 +1,17 @@
+package com.github.jrh3k5.plugin.maven.l10n.util.internal;
+
+/**
+ * A simple interface for testing purposes.
+ * 
+ * @author Joshua Hyde
+ * @since 1.3
+ */
+
+public interface TestInterface {
+    /**
+     * Get the name.
+     * 
+     * @return The name.
+     */
+    String name();
+}
---
+++ src/test/java/com/github/jrh3k5/plugin/maven/l10n/util/internal/TestReflection.java
@@ -0,0 +1,13 @@
+package com.github.jrh3k5.plugin.maven.l10n.util.internal;
+
+
+/**
+ * Test enumerations implementing {@link TestInterface}.
+ * 
+ * @author Joshua Hyde
+ * @since 1.3
+ */
+
+public enum TestReflection implements TestInterface {
+    A, B;
+}
